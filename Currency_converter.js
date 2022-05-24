11b5662950d261fbcbc2497a2a6e516a6b5cdbc4
Daniel Irimia <91692405+danielirimia60@users.noreply.github.com>
2022-05-24 08:45:39 +0100

    Update Currency_converter.js

--- Currency_converter.js
+++ Currency_converter.js
@@ -1,3 +1,14 @@
 //Currency exchange calculator
 
-const dollarToGbp = document.querySelector('[data-type="dollar"]');
+const from = document.getElementById('from');
+const to = document.getElementById('to');
+
+const fromValue = document.getElementById('from').value;
+const toValue = document.getElementById('to').value;
+
+const swap = () => {
+    if(fromValue == 'usd' && toValue == 'gbp') {
+        from = fromValue;
+        toValue = 'usd';
+    }
+}
